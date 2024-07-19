706b2a8995d91a711c02e7d66ef1734709dcb8e6
brick <patchouliknowledge1998@proton.me>
2024-07-19 21:15:11 +0700

    Find function

--- termscrollsql.py
+++ termscrollsql.py
@@ -10,7 +10,9 @@ status_msg = "Welcome to termscroll! Type \"h\" to see all commands!"
 
 random_output_max : int = 5
 _random_output : bool = False # Bool var for randomized output. 
-
+_find_results_output : bool = False # Bool for results of search
+_search_results = [] # Search result array. Used by function.
+_search_word : str = "Hello" # Find function global input.
 # Making a database:
 with sqlite3.connect(FileName) as DataBase:
     pass
@@ -25,24 +27,45 @@ crs.execute("""CREATE TABLE IF NOT EXISTS goals(
 
 # Functions: -------------------------------------------------------------------------
 def ShowDB_Default():
-    if(_random_output):
+
+    if(_find_results_output):
+        crs.execute("SELECT rowid, * FROM goals") # Find results output
+        all_tasks = crs.fetchall()
+        task_string : str = ""
+        start_index : int = 0
+        _search_results.clear()
+        for task in all_tasks:
+            if _search_word.lower() in task[1].lower(): _search_results.append(f"[{task[0]}]: {task[1]}")
+
+
+        if (len(_search_results) == 0):
+            print("No matches found.")
+        else: 
+            for i in _search_results:
+                print(i)
+
+
+
+
+
+    if(_random_output and not _find_results_output): # Random output
         crs.execute("SELECT rowid, * FROM goals")
         all_tasks = crs.fetchall()
         rand_queue = []
         for i in range(random_output_max):
             rand_temp : int = random.randint(0, len(all_tasks)-1)
             if (rand_queue.count(rand_temp) == 0): rand_queue.append(rand_temp)
-        for i in rand_queue:
+        for i in rand_queue: 
             print(f"[{i+1}]: {all_tasks[i][1]}")
 
-
-
-
-    else:
+    elif(not _random_output and not _find_results_output): # Default output
         crs.execute("SELECT rowid, * FROM goals")
         items = crs.fetchall()
         for i in items:
             print(f"[{i[0]}]: {i[1]}")
+        print(len(items)) ##############
+    
+
 
 def ShowStatus(msg):
     global status_msg
@@ -108,8 +131,21 @@ while(True):
             ShowStatus(f"[{inp}]: > {Edit(inp)} < was changed.")
 
         case "h":
-            ShowStatus("a(dd), s(ave), l(oad), r(emove), (e)dit, (h)elp, (rand)om output, (def)ault output",)
+            ShowStatus("a(dd), s(ave), l(oad), r(emove), (e)dit, (h)elp, (rand)om output, (def)ault output, f(ind)",)
 
         case "rand":
             ShowStatus("Random mode activated")
             _random_output = True
+            _find_results_output = False
+        
+        case "def":
+            ShowStatus("Default mode activated")
+            _random_output = False
+            _find_results_output = False
+        
+        case "f":
+            ShowStatus("Enter words to find: ")
+            _find_results_output = True
+            _random_output = False
+            _search_word = input("Word to find: ")
+             
